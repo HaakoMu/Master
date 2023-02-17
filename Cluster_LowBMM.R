39dd6772dbb05e19325e2f5ce9fb2c7fed6495ca
HaakoMu <71719368+HaakoMu@users.noreply.github.com>
2023-02-17 12:11:01 +0100

    small changes, addition of rank consistency

--- Cluster_LowBMM.R
+++ Cluster_LowBMM.R
@@ -3,7 +3,7 @@ library(Rcpp)
 library(dplyr)
 library(gtools)
 library(plotly)
-sourceCpp("C:/Users/Haako/OneDrive/Documents/UiO/Master/Cluster LOWBMM/lowBMM/Cpp/leapandshift_sourceCpp.cpp") # use the C++ leap-and-shift function
+sourceCpp("C:/Users/Haako/OneDrive/Documents/UiO/Master/Cluster LOWBMM/Cluster_LowBMM/Master/Cpp/leapandshift_sourceCpp.cpp") # use the C++ leap-and-shift function
 source("MCMC.R")
 source("Simulations.R")
 source("Results.R")
@@ -11,12 +11,12 @@ library(ggplot2)
 
 
 ##### Fixing parametes #####
-N <- 80 # number of assessors 
+N <- 20 # number of assessors 
 n <- 40 # total number of items
 n_star <- 20 # number of items selected to be "relevant", i.e. will have the highest ranks
 n_star_true <- n_star
 alpha_true <- alpha0 <- 2
-C <- 4 #number of clusters
+C <- 2 #number of clusters
 thinning = 10 
 
 # Tuning parmameters for the MCMC
@@ -28,8 +28,9 @@ prob_back <- prob_forw <- 0.5
 burnin <- 0.3
 A_star <- matrix(c(1,20,2,19,3,18,4,17,5,16,6,15,7,14,8,13),nrow = 2)
 
+simulation <- sim_rank_consistency(n,N,C,n_star)
 
-simulation <- sim_topK(n,N,C,n_star)
+#simulation <- sim_topK(n,N,C,n_star)
 
 init <- generate_random_init(M,N,C,n_star)
 
@@ -38,6 +39,52 @@ init <- generate_random_init(M,N,C,n_star)
 mcmc <- cluster_mcmc(simulation$data, M, C, N, n_star, alpha0, init$rho0, init$A_star0, init$clusters, prob_back, prob_forw)
 
 results <- result_matrix(burnin_percentage = burnin, cluster_mcmc = mcmc$clusters, C, true_index_cluster =simulation$true_index_cluster,rho_mcmc= mcmc$rho_mcmc,A_mcmc = mcmc$A_mcmc, n_star,true_rank = simulation$true_ranks)
+
+
+trace <- trace_plot(mcmc$clusters)
+
+boxplot_mat <- boxplot_cluster(mcmc$clusters)
+boxplot(t(boxplot_mat))
+boxplot_mat
+
+
+
+
+
+fix_burnin <- function(x){
+  m <- nrow(x)
+  b <<- burnin
+  print(x)
+  print(m)
+  return(x[m*b:m,])
+}
+asd <- mcmc$A_mcmc
+fff <- nrow(asd[[1]])*burnin
+A_post <- lapply(asd, function(x) x[-c(1:fff), ])
+
+
+my_matrix <- A_post[[1]]
+paste(my_matrix[1,], collapse = ",")
+
+
+count_unique_strings <- function(x) {
+  paste(my_matrix[1,], collapse = ",")
+}
+
+# Apply the custom function to each row of the matrix
+string_counts <- apply(my_matrix, 1, count_unique_strings)
+MAP <- as.integer(strsplit((dimnames(my_table)[[1]][which.max(table(string_counts))]), ",")[[1]])
+
+
+out <- matrix(nrow=1, ncol=0)
+
+for(i in 1:n){
+  col <- matrix(sum(my_matrix==i)/nrow(my_matrix), ncol = 1)
+  colnames(col) <- as.character(i)
+  out <- cbind(out, col,deparse.level = 0)
+}
+
+barplot(out[, rev(order(out[1,]))[1:n_star]], main = "Probability of items from MAP", xlab = "Column Index", ylab = "Value")
 
 
 
--- Simulations.R
+++ Simulations.R
@@ -28,6 +28,34 @@ sim_topK <- function(n, N, C, n_star, true_rho = NULL, true_A_star = NULL){
 }
 
 
+sim_rank_consistency <- function(n, N, C, n_star, true_rho = NULL, true_A_star = NULL){
+  true_ranks <- matrix(NA, nrow = C, ncol= n_star)
+  rho_sim <- is.null(true_rho)
+  if(rho_sim) true_rho <- matrix(NA, nrow = C, ncol= n_star)
+  A_sim <- is.null(true_A_star)
+  if(A_sim)true_A_star <- matrix(NA, nrow = C, ncol= n_star)
+  true_index_cluster <-matrix(NA, nrow = C, ncol=N/C)
+  data <- NULL
+  for(i in 1:C){
+    N_C = N/C
+    if(rho_sim) rho_true <- sample.int(n = n_star, size = n_star) else rho_true <- true_rho[i,]
+    if(A_sim) A_star_true <- sort(sample.int(n = n, size = n_star)) else  A_star_true <- true_A_star[i,]
+    data_red <- sample_mallows(rho_true, alpha_true, N_C, burnin = 1e4, thinning = 100)
+    true_rank <- A_star_true[sort(rho_true, index.return=TRUE)$ix]
+    rho_true_ordered <- sort(rho_true, index.return =TRUE)$ix
+    for(j in 1:N_C)data_red[j,rho_true_ordered] <- sort(sample(1:n, size = n_star))
+    data_c <- matrix(NA, N_C, n) 
+    data_c[,A_star_true] <- data_red
+    true_ranks[i,] <- true_rank
+    true_A_star[i,] <- A_star_true
+    true_rho[i,] <- rho_true
+    for(j in 1:N_C)data_c[j,setdiff(1:n, A_star_true)] <- sample(setdiff(1:n, data_red[j,]), size = n-n_star)
+    true_index_cluster[i,] <- c(1:N_C)+(N_C*(i-1))
+    data <- rbind(data,data_c)
+  }
+  return(list(data = data, true_rho =true_rho, true_A_star=true_A_star, true_ranks = true_ranks, true_index_cluster= true_index_cluster ))
+}
+
 generate_random_init <- function(M,N,C,n_star, thinning = 10){
   #Clusters initializer 
   clusters <- matrix(NA, floor(M/thinning), N)
